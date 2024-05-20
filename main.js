e4ae5cb35e7d6ad45ee4508a3e5b83416d26824a
sebas4rg93n70 <sebastian_reinoso@hotmail.com>
2024-05-20 15:04:45 -0300

    FIX width card y sistema de color de poke card

--- main.js
+++ main.js
@@ -8,34 +8,12 @@ export const containerCards = document.querySelector(".container_cards");
 // CONSOLE
 
 // FUNCIONES
-const typeColorPoke = (typePoke) => {
-  const colors = {
-    electric: "#e0e12a",
-    normal: "#a8a878",
-    fire: "#f08030",
-    water: "#6890f0",
-    grass: "#78c850",
-    ice: "#98d8d8",
-    fighting: "#c03028",
-    poison: "#a040a0",
-    ground: "#e0c068",
-    flying: "#a890f0",
-    psychic: "#f85888",
-    bug: "#a8b820",
-    rock: "#b8a038",
-    ghost: "#705898",
-    dragon: "#705898",
-    dark: "#040608",
-    steel: "#b8b8d0",
-    fairy: "#f0b6bc",
-  };
-  return colors[typePoke];
-};
+
 const dataCardPoke = (poke) => {
   return {
     titleName: poke.name.toUpperCase(),
     image: poke.sprites.front_default,
-    type: poke.types[0].type.name.toUpperCase(),
+    type: poke.types.map((item) => item.type.name.toUpperCase()).join(" / "),
     weigth: poke.weight / 10,
     height: poke.height / 10,
     hp: poke.stats[0].base_stat,
@@ -47,7 +25,6 @@ const dataCardPoke = (poke) => {
   };
 };
 const templateCardPoke = (poke) => {
-  let typeColor = poke.types[0].type.name;
   let {
     titleName,
     image,
@@ -61,7 +38,7 @@ const templateCardPoke = (poke) => {
     superDefense,
     speed,
   } = dataCardPoke(poke);
-  return `<div class="card" style=background-color:${typeColorPoke(typeColor)}>
+  return `<div class="card  ${type.toLowerCase().split("/")[0].trim()}"   >
   <div class="title_name">
     <h3>${titleName}</h3>
   </div>
@@ -90,12 +67,13 @@ const findPokeCard = async (e) => {
   e.preventDefault();
   const idPoke = inputFind.value.trim();
   if (!idPoke) {
-    containerCards.innerHTML = "Debe ingresar un número para buscar un Pokemon";
+    containerCards.innerHTML =
+      "Debes ingresar un número para buscar un Pokemon";
     formFind.reset();
     return;
   }
   if (idPoke <= 0 || idPoke > 1302) {
-    containerCards.innerHTML = "Los #ID de los Pokemons   van del 1 al 1302";
+    containerCards.innerHTML = "Los #ID de los Pokemones   van del 1 al 1302";
     formFind.reset();
     return;
   }
@@ -106,5 +84,6 @@ const findPokeCard = async (e) => {
 // INIT
 const init = () => {
   formFind.addEventListener("submit", findPokeCard);
+  inputFind.focus();
 };
 init();
